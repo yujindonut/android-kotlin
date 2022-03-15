bd5dba23ac7903dd683dad0fd66d37134739c394
yujindonut <bbb3631@naver.com>
2022-03-15 20:44:43 +0900

    [add] google_map buttons

--- app/src/main/java/work/risingcamp/week9/MainActivity.kt
+++ app/src/main/java/work/risingcamp/week9/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
             }
             else if (tokenInfo != null) {
                 Toast.makeText(this, "토큰 정보 보기 성공", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, SecondActivity::class.java)
+                val intent = Intent(this, MapsActivity::class.java)
                 startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
                 finish()
             }
